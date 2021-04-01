8a575a327255a33e19e33732d2b3845fedebeac6
VeronikaMishkovec <veronika.mishkovec@gmail.com>
2021-04-01 17:02:41 +0300

    types for saga

--- src/store/saga/listSaga.tsx
+++ src/store/saga/listSaga.tsx
@@ -1,13 +1,14 @@
-import { put, call, takeLatest, takeEvery } from 'redux-saga/effects';
+import { put, call, takeLatest } from 'redux-saga/effects';
 import { getNewsList } from '../service';
 import { GET_NEWS_LIST, SET_NEWS_LIST } from '../actions/action';
+import { List } from '../type';
 
 function* newsList() {
-  const payload: {} = yield call(getNewsList);
-  const res: {} = yield put({ type: SET_NEWS_LIST, payload });
+  const payload: List[] = yield call(getNewsList);
+  const res: Object = yield put({ type: SET_NEWS_LIST, payload });
   return res;
 }
 
 export default function* newsListSaga() {
-  yield takeEvery(GET_NEWS_LIST, newsList);
+  yield takeLatest(GET_NEWS_LIST, newsList);
 }
