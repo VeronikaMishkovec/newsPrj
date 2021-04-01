cd58effe60ffa603ae776dd64e62f7fffe8449a4
VeronikaMishkovec <veronika.mishkovec@gmail.com>
2021-04-01 12:49:15 +0300

    add axios

--- src/store/service.tsx
+++ src/store/service.tsx
@@ -1,7 +1,8 @@
+import axios from 'axios';
+
 export const getNewsList = async () => {
-  const res = await fetch(
-    'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=cEWFEXyy9FAG4sCXACpPK4XYRp44Uenr',
-  );
-  const data = await res.json();
-  return data.results;
-}; 
+  const apiURL =
+    'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=cEWFEXyy9FAG4sCXACpPK4XYRp44Uenr';
+  const data = axios.get(apiURL).then(resp => resp.data.results);
+  return data;
+};
