df0fb1b0099214caca53fcac4ec366cadb58555a
VeronikaMishkovec <veronika.mishkovec@gmail.com>
2021-04-05 12:24:16 +0300

    add file with constants

--- src/components/Link/styles.ts
+++ src/components/Link/styles.ts
@@ -1,17 +1,18 @@
 import { StyleSheet } from "react-native";
+import { COLOR, FONT, FONT_SIZE, BORDER_RADIUS, PADDING } from "../constants";
 
 import { LinkStylesTypes } from "./types";
 
 export const styles = StyleSheet.create<LinkStylesTypes>({
   button: {
     alignItems: 'center',
-    backgroundColor: '#72a2ac',
-    borderRadius: 10,
-    paddingVertical: 10,
+    backgroundColor: COLOR.BTN,
+    borderRadius: BORDER_RADIUS,
+    paddingVertical: PADDING,
   },
   buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 18,
+    color: COLOR.LIGHT_TEXT,
+    fontFamily: FONT.BOLD,
+    fontSize: FONT_SIZE.HEADER,
   },
 })
